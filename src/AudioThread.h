824a97504645821d0c92d82582626af1f9b67ab2
Amoulier <Amoulier@gmail.com>
2026-08-22 14:07:17 -0400

    fix: track shared RTTTL playback ownership

--- src/AudioThread.h
+++ src/AudioThread.h
@@ -22,9 +22,11 @@ extern ExtensionIOXL9555 io;
 class AudioThread : public concurrency::OSThread
 {
   public:
+    enum class RtttlOwner : uint8_t { NONE, SYSTEM, EXTERNAL_NOTIFICATION };
+
     AudioThread() : OSThread("Audio") { initOutput(); }
 
-    void beginRttl(const void *data, uint32_t len)
+    void beginRttl(const void *data, uint32_t len, RtttlOwner owner = RtttlOwner::SYSTEM)
     {
 #ifdef T_LORA_PAGER
         io.digitalWrite(EXPANDS_AMP_EN, HIGH);
@@ -32,6 +34,7 @@ class AudioThread : public concurrency::OSThread
         setCPUFast(true);
         rtttlFile = std::unique_ptr<AudioFileSourcePROGMEM>(new AudioFileSourcePROGMEM(data, len));
         i2sRtttl = std::unique_ptr<AudioGeneratorRTTTL>(new AudioGeneratorRTTTL());
+        rtttlOwner = owner;
         i2sRtttl->begin(rtttlFile.get(), audioOut.get());
     }
 
@@ -39,9 +42,22 @@ class AudioThread : public concurrency::OSThread
     bool isPlaying()
     {
         if (i2sRtttl != nullptr) {
-            return i2sRtttl->isRunning() && i2sRtttl->loop();
+            const bool playing = i2sRtttl->isRunning() && i2sRtttl->loop();
+            if (!playing) {
+                stop();
+            }
+            return playing;
         }
         return false;
+    }
+
+    bool stopRtttlIfOwnedBy(RtttlOwner owner)
+    {
+        if (rtttlOwner != owner) {
+            return false;
+        }
+        stop();
+        return true;
     }
 
     void stop()
@@ -52,6 +68,7 @@ class AudioThread : public concurrency::OSThread
         }
 
         rtttlFile = nullptr;
+        rtttlOwner = RtttlOwner::NONE;
 
         setCPUFast(false);
 #ifdef T_LORA_PAGER
@@ -65,6 +82,7 @@ class AudioThread : public concurrency::OSThread
             i2sRtttl->stop();
             i2sRtttl = nullptr;
         }
+        rtttlOwner = RtttlOwner::NONE;
 
 #ifdef T_LORA_PAGER
         io.digitalWrite(EXPANDS_AMP_EN, HIGH);
@@ -100,6 +118,7 @@ class AudioThread : public concurrency::OSThread
     std::unique_ptr<AudioOutputI2S> audioOut = nullptr;
 
     std::unique_ptr<AudioFileSourcePROGMEM> rtttlFile = nullptr;
+    RtttlOwner rtttlOwner = RtttlOwner::NONE;
 };
 
 #endif
